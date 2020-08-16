0b7f756ee1b3a3535ad1d7182239b9ba3cd27de7
Aravindakumar <aravind.aurobindo@gmail.com>
2020-08-16 12:37:19 +0530

    com2

---
+++ gitTry/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitTry
+//
+//  Created by Aravindakumar on 16/08/20.
+//  Copyright © 2020 Aravindakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello git")
+    }
+
+
+}
+
