04629bb146be77a7d4d9d9c8cac2e1b64dc70907
Aravindakumar <aravind.aurobindo@gmail.com>
2020-08-16 12:56:03 +0530

    remote

--- gitTry/ViewController.swift
+++ gitTry/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hello git")
         print("change 2")
+        print("remote")
     }
 
 
